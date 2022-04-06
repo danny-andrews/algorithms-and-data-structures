dd2eb0645e0eb141aa180facee78dc2d9782b5e8
Danny <lunde.andrews@gmail.com>
2022-04-06 11:16:07 -0400

    Add both solutions for matrix rotation

--- src/algorithms/matrices/rotate-in-place.js
+++ src/algorithms/matrices/rotate-in-place.js
@@ -1,3 +1,24 @@
+const rotate = (matrix) => {
+  const size = matrix.length;
+  const newMatrix = [];
+  for (let col = 0; col < size; col++) {
+    newMatrix.push([]);
+    for (let row = size - 1; row >= 0; row--) {
+      newMatrix[col].push(matrix[row][col]);
+    }
+  }
+
+  return newMatrix;
+};
+
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+const result = rotate(matrix);
+console.log(result);
+
 const rotateInPlace = (matrix) => {
   const size = matrix.length;
   for (let i = 0; i < size; i++) {
